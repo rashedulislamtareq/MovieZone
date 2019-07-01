dd283f1443ecdc1103dcc358b228a175e9742f63
Rashedul Islam <rashed.tareq@live.com>
2019-07-01 20:18:27 +0600

    Added Index to View Movies & Customers

---
+++ MovieZone/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using MovieZone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieZone.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer(){ Id = 1, Name = "Customer 1"},
+                new Customer(){ Id = 2, Name = "Customer 2"}
+            };
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer(){ Id = 1, Name = "Customer 1"},
+                new Customer(){ Id = 2, Name = "Customer 2"}
+            };
+
+            var customer = customers.FirstOrDefault(x => x.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+
+        }
+    }
+}
---
+++ MovieZone/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using MovieZone.Models;
+using MovieZone.Models.ViewModels;
+
+namespace MovieZone.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie(){ Id = 1, Name = "Titanic"},
+                new Movie(){ Id = 2, Name = "Dark"}
+            };
+
+            return View(movies);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Titanic" };
+            var customers = new List<Customer>()
+            {
+                new Customer(){ Id = 1, Name = "Customer 1"},
+                new Customer(){ Id = 2, Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+    }
+}
