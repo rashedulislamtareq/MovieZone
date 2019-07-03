94e6497d04a1be859dfda7ca8d4bb9709b99b8ee
Rashedul Islam <rashed.tareq@live.com>
2019-07-03 12:31:00 +0600

    Enabled Web API & added Customers Web API Controller

---
+++ MovieZone/Controllers/API/CustomersController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Hosting;
+using System.Web.Http;
+using MovieZone.Models;
+
+namespace MovieZone.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/customers
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(x => x.Id == id);
+
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        //POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        //PUT /api/customer/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerToUpdate = _context.Customers.FirstOrDefault(x => x.Id == id);
+
+            if (customerToUpdate == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerToUpdate.Birthdate = customer.Birthdate;
+            customerToUpdate.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerToUpdate.MembershipTypeId = customer.MembershipTypeId;
+            customerToUpdate.Name = customer.Name;
+
+            _context.SaveChanges();
+        }
+
+        //DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerToDelete = _context.Customers.FirstOrDefault(x => x.Id == id);
+            if (customerToDelete == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerToDelete);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
